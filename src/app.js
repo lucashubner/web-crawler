8878d0d719bfc2876fa02c417475c8cc54e0ecd0
Lucas Hubner <lucasguilhermehub@gmail.com>
2021-04-07 02:18:08 -0300

    Adapting crawler to function so it can be parallelized

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const puppeteer = require('puppeteer');
-const apimonitor = require('./monitorAPIS').apimonitor
+const crawler = require('./crawler')
+
 const fs = require('fs');
 
 function delay(time) {
@@ -14,45 +14,14 @@ function delay(time) {
 	let rawData = fs.readFileSync('sites.json');
 	let sitesObj = JSON.parse(rawData);
 	
-	// Args --no-sandbox --disable-gpu necessary to run on docker environment
-	const browser = await puppeteer.launch({
-		headless: true,
-		args: [
-			"--no-sandbox", 
-			"--disable-gpu",
-		]
-	});
 
 	var result = [];
-	for ( let site in sitesObj.sites) {
-		console.log("Oppening page");
-		console.log(sitesObj.sites[site]);
-
-		const page = await browser.newPage();
-		await page.setDefaultNavigationTimeout(0); 
-		await page.evaluateOnNewDocument(apimonitor);
-
-
-		await page.goto(sitesObj.sites[site]);
-
-		// console.log("Waiting 5 seconds");
-		// await delay (5000);
-
-		const monitored = await page.evaluate(() => {
-			return new Promise(resolve => {
-				setTimeout(() => {
-					resolve (navigator.monitorAPI);
-				}, 200);
-			});
-		});
-		monitored['site'] = sitesObj.sites[site];
-		result.push(monitored);
-		page.close();
+	for ( let site in sitesObj.sites ) {
+		result.push (crawler.crawl(sitesObj.sites[site]));
 	}
 
 	let data = JSON.stringify(result);
 	
 	await fs.writeFileSync('out/results_'+ Date.now()+'.json', data);
-	await browser.close();
 
 })();
---
+++ src/crawler.js
@@ -0,0 +1,53 @@
+const crawler =  (function () {
+const puppeteer = require('puppeteer');
+const apimonitor = require('./monitorAPIS').apimonitor
+
+	function delay(time) {
+	   return new Promise(function(resolve) { 
+		   setTimeout(resolve, time)
+	   });
+	}
+	const crawl = async function (site){
+		// Args --no-sandbox --disable-gpu necessary to run on docker environment
+		const browser = await puppeteer.launch({
+			headless: true,
+			args: [
+				"--no-sandbox", 
+				"--disable-gpu",
+			]
+		});
+
+		console.log("Oppening page");
+		console.log(site);
+
+		const page = await browser.newPage();
+		await page.setDefaultNavigationTimeout(0); 
+		await page.evaluateOnNewDocument(apimonitor);
+
+		await page.goto(site);
+
+		console.log("Waiting 5 seconds");
+		await delay (5000);
+
+		const monitored = await page.evaluate(() => {
+			return new Promise(resolve => {
+				setTimeout(() => {
+					resolve (navigator.monitorAPI);
+				}, 200);
+			});
+		});
+
+		monitored['site'] = site
+
+		page.close();
+		await browser.close();
+		return monitored;
+	}
+
+	return crawl;
+}) ();
+
+module.exports = {
+	crawl : crawler
+};
+
